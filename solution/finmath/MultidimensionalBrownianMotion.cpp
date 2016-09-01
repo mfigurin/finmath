ca99d54124c7eaa007eca4ac59ab7ea6aabf6c65
ShklyarovE <evgeny.shklyarov@gmail.com>
2016-09-01 17:31:36 +0300

    added BrownianMotion

---
+++ solution/finmath/MultidimensionalBrownianMotion.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "MultidimensionalBrownianMotion.h"
+
+
+MultidimensionalBrownianMotion::MultidimensionalBrownianMotion(void)
+{
+}
+
+
+MultidimensionalBrownianMotion::~MultidimensionalBrownianMotion(void)
+{
+}
