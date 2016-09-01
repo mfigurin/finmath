7065e9325c6c09bb2eb81c89abb139640fcafc00
ShklyarovE <evgeny.shklyarov@gmail.com>
2016-09-01 20:05:05 +0300

    added MultidimensionalBrownianMotion.cpp

--- solution/finmath/MultidimensionalBrownianMotion.cpp
+++ solution/finmath/MultidimensionalBrownianMotion.cpp
@@ -1,12 +1,107 @@
 #include "stdafx.h"
 #include "MultidimensionalBrownianMotion.h"
 
-
-MultidimensionalBrownianMotion::MultidimensionalBrownianMotion(void)
+namespace Sample
 {
+
+ShareInfo::ShareInfo(void) {}
+ShareInfo::ShareInfo(double S0, double mu, double sigma) : S0(S0), sigma(sigma), nu(mu-sigma*sigma/2) {};
+
+ShareInfo::ShareInfo(const ShareInfo& info)
+{
+	this->operator=(info);
+};
+
+void ShareInfo::operator=(const ShareInfo& info)
+{
+	S0 = info.S0;
+	sigma = info.sigma;
+	nu = info.nu;
 }
 
-
-MultidimensionalBrownianMotion::~MultidimensionalBrownianMotion(void)
+double ShareInfo::get_S0() const
 {
+	return S0;
+}
+
+double ShareInfo::get_sigma() const
+{
+	return sigma;
+}
+
+double ShareInfo::get_nu() const
+{
+	return nu;
+}
+
+Matrix<double>* MultidimensionalBrownianMotion::Simulate(vector<ShareInfo> shares, vector<double> Corr, int T, int steps, int sims)
+{
+	int numAssets = shares.size();
+	double dt = 1.0/252;
+
+	vector<double> nu = vector<double>(numAssets);
+	vector<double> sigma = vector<double>(numAssets);
+
+	for(int i=0; i<numAssets; ++i) {
+		nu[i] = shares[i].get_nu();
+		sigma[i] = shares[i].get_sigma();
+	}
+
+	//Cholesky decomposition of correlation matrix to produce upper triangular matrix
+	//Matrix Y = transpose(cholesky(Corr), numAssets, numAssets);
+	CholeskyUpperTriangularMatrix<double> Y = CholeskyUpperTriangularMatrix<double>(Corr, numAssets);
+
+	//populate matrix of size steps by numAssets with nu*dt
+	//Matrix nudt(steps*sims, vector<double>(numAssets));
+	Matrix<double> nudt = Matrix<double>(steps*sims, numAssets);
+
+	for (int i = 0; i < steps*sims; ++i) {
+		for (int j = 0; j < numAssets; ++j) {
+			nudt(i,j) = nu[j]*dt;
+		}
+	}
+
+	//convert vector sigma into diagonal matrix with zeros on off-diagonals
+	Matrix<double> diagSig = Matrix<double>(numAssets, numAssets);
+	for(int i=0; i<numAssets; ++i) {
+		for(int j=0; j<numAssets; ++j) {
+			diagSig(i,j) = i == j ? sigma[i] : 0.0;
+		}
+	}
+
+	//Multidimensional Brownian Motion Simulation Begins
+
+	srand((unsigned)time(0));
+
+	//Matrix randnMatrix(steps*sims, vector<double> (numAssets));
+	Matrix<double> randnMatrix(steps*sims, numAssets);
+
+	NormalDistribution random_normal;
+
+	for (int i = 0; i < steps*sims; i++) {
+		for (int j = 0; j < numAssets; j++) {
+			//populate random matrix
+			randnMatrix(i,j) = random_normal.nextValue();
+		}
+	}
+
+	//correlate the random variables
+	//Matrix Z = matrixMult(randnMatrix, Y, steps*sims, numAssets, numAssets);
+	//Matrix C = matrixMult(Z, diagSig, steps*sims, numAssets, numAssets);
+	//Matrix vPrices(steps*sims, vector<double> (numAssets));
+
+	Matrix<double> Z = randnMatrix * Y;
+	Matrix<double> C = Z * diagSig;
+	Matrix<double>* vPrices = new Matrix<double>(steps*sims, numAssets);
+
+	for (int i = 0; i < steps*sims-1; i++) {
+		for (int j = 0; j < numAssets; j++) {
+			//initialize new paths with initial asset prices or generate full paths
+			(*vPrices)(i,j) = i==0 || (i+1)%252==0 ? shares[j].get_S0() : exp(nudt(i,j) + C(i,j)*sqrt(dt)) * (*vPrices)(i-1,j);
+		}
+	}
+
+	return vPrices;
+}
+
 }
--- solution/finmath/MultidimensionalBrownianMotion.h
+++ solution/finmath/MultidimensionalBrownianMotion.h
@@ -5,125 +5,39 @@
 #include <iomanip>
 #include <vector>
 #include "Matrix.h"
+#include "randomgenerator.h"
 
 using namespace std;
 
 namespace Sample {
 
-class Share
+class ShareInfo
 {
-	public:
-		const double S0;
-		const double mu;
-		const double sigma;
-		const double nu;
+private:
+	double S0;
+	double sigma;
+	double nu;
 
-		Share(double S0, double mu, double sigma) : S0(S0), mu(mu), sigma(sigma), nu(mu-sigma*sigma/2) {};
-	};
+public:
+	ShareInfo(void);
+	ShareInfo(double S0, double mu, double sigma);
+	ShareInfo(const ShareInfo& info);
 
-	class MultidimensionalBrownianMotion
-	{
-	public:
-		MultidimensionalBrownianMotion(void);
-		~MultidimensionalBrownianMotion(void);
+	void operator=(const ShareInfo& info);
 
-		//S0: initial price vector
-		//mu: mean vector
-		//sigma: standard deviation vector
-		//Corr: correlation matrix
-		//T: time (in years)
-		//numAssets: number of assets
-		//steps: number of days
-		//sims: number of iterations
-		Matrix<double> multiBrownianMotion(
-			vector<Share> shares,
-			vector<double> Corr,
-			int T,
-			int steps,
-			int sims)
-		{
-			int numAssets = shares.size();
-			double dt = 1.0/252;
+	double get_S0() const;
+	double get_sigma() const;
+	double get_nu() const;
+};
 
-			vector<double> nu = vector<double>(numAssets);
-			vector<double> sigma = vector<double>(numAssets);
-
-			for(int i=0; i<numAssets; ++i) {
-				nu[i] = shares[i].nu;
-				sigma[i] = shares[i].sigma;
-			}
-
-			//Cholesky decomposition of correlation matrix to produce upper triangular matrix
-			//Matrix Y = transpose(cholesky(Corr), numAssets, numAssets);
-			CholeskyUpperTriangularMatrix<double> Y = CholeskyUpperTriangularMatrix<double>(Corr, numAssets);
-
-			//populate matrix of size steps by numAssets with nu*dt
-			//Matrix nudt(steps*sims, vector<double>(numAssets));
-			Matrix<double> nudt = Matrix<double>(steps*sims, numAssets);
-
-			for (int i = 0; i < steps*sims; ++i) {
-				for (int j = 0; j < numAssets; ++j) {
-					nudt(i,j) = nu[j]*dt;
-				}
-			}
-
-			//convert vector sigma into diagonal matrix with zeros on off-diagonals
-			Matrix diagSig = diag(sigma, numAssets);
-
-			//Multidimensional Brownian Motion Simulation Begins
-
-			srand((unsigned)time(0));
-
-			//Matrix randnMatrix(steps*sims, vector<double> (numAssets));
-			Matrix<double> randnMatrix(steps*sims, numAssets);
-
-			for (int i = 0; i < steps*sims; i++) {
-				for (int j = 0; j < numAssets; j++) {
-					//populate random matrix
-					randnMatrix(i,j) = random_normal();
-				}
-			}
-
-			//correlate the random variables
-			//Matrix Z = matrixMult(randnMatrix, Y, steps*sims, numAssets, numAssets);
-			//Matrix C = matrixMult(Z, diagSig, steps*sims, numAssets, numAssets);
-			//Matrix vPrices(steps*sims, vector<double> (numAssets));
-
-			Matrix<double> Z = randnMatrix * Y;
-			Matrix<double> C = Z * diagSig;
-			Matrix<double> vPrices = Matrix<double>(steps*sims, numAssets);
-
-			for (int i = 0; i < steps*sims-1; i++) {
-				for (int j = 0; j < numAssets; j++) {
-					//initialize new paths with initial asset prices or generate full paths
-					vPrices(i,j) = i==0 || (i+1)%252==0 ? shares[j].S0 : exp(nudt(i,j) + C(i,j)*sqrt(dt)) * vPrices(i-1,j);
-				}
-			}
-
-			return vPrices;
-		};
-
-		//Takes in vector and writes its entires on the diagonal of a matrix with zeros elsewhere
-		Matrix diag(vector<double> V, int n)
-		{
-			vector<vector<double> > ans(n, vector<double> (n));
-			for (int j=0; j< n; j++)
-			{
-				for (int i=0; i<n; i++)
-				{
-					ans[i][j] = 0;
-				}
-			}
-
-			for (int j=0; j< n; j++)
-			{
-				for (int i=0; i<n; i++)
-				{
-					ans[i][i] = V[i];
-				}
-			}
-			return ans;
-		}
-	};
+class MultidimensionalBrownianMotion
+{
+public:
+	//Corr: correlation matrix
+	//T: time (in years)
+	//steps: number of days
+	//sims: number of iterations
+	static Matrix<double>* Simulate(vector<ShareInfo> shares, vector<double> Corr, int T, int steps, int sims);
+};
 
 }
