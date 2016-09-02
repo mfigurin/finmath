0e3730dbfcdf224f3c9fa8e53b7c41bd230fd857
mfigurin <mikhail.figurin@gmail.com>
2016-09-02 23:53:37 +0300

    Matrix.cpp added

---
+++ solution/finmath/Matrix.cpp
@@ -0,0 +1,72 @@
+#include "stdafx.h"
+#include "Matrix.h"
+
+namespace Sample {
+
+	Matrix::Matrix( int rows, int cols, double fill){
+		assert( rows > 0 && cols > 0 );
+		_matrix = std::vector<std::vector<double>>( rows, std::vector<double>(cols, fill));
+	}
+
+	Matrix::Matrix( const Matrix& matrix ) {
+		_matrix = matrix._matrix;
+	}
+
+	Matrix Matrix::operator* ( const Matrix& matrix ) {
+		assert( cols() == matrix.rows());
+		Matrix mult( rows(), matrix.cols() );
+		for( int i = 0; i < mult.rows(); i++ ) {
+			for( int j  = 0; j < mult.cols(); j++ ) { 
+				for( int k = 0; k < cols(); k++ ) {
+					mult(i,j) += (*this)(i,k) * matrix(k,j);
+				}
+			}
+		}
+		return mult;
+	}
+
+	Matrix Matrix::transpose(void) 
+	{
+		Matrix transposed( cols(), rows() );
+		for (int i = 0; i < rows(); i++) {
+			for (int j = 0; j < cols(); j++)
+				transposed(j,i) = (*this)(i,j);
+		}
+		return transposed;
+	}
+
+	Matrix Matrix::cholesky_decomposition() 
+	{
+		Matrix l(rows(),cols());
+
+		for( int i = 0; i < rows(); i++){
+			for( int j = 0; j <= i; j++) {
+				double sum = 0.;
+				for( int k = 0; k < j; k++ ) {
+					sum += l(i,k) * l(j,k); // alg: i == j ?  l(j,k) * l(j,k) : l(i,k) * l(j,k);
+				}
+				double v = (*this)(i,j) - sum;
+				if( i == j ) {
+					if( v < 0. ) {
+						throw std::exception( "The matrix is not positive" );
+					}
+					l(j,j) = sqrt( v ); // alg: sqrt( this->(j,j) - sum );
+				}else{
+					l(i,j) = v / l(j,j); // alg: (1./l(j,j)) * (this->(i,j) - sum);
+				}
+			}
+		}
+		return l;
+	}			
+
+	CorrelationMatrix::CorrelationMatrix( unsigned int size, double fill) : Matrix(size,size,fill) {
+		for (unsigned int i = 0; i < size; i++)
+			(*this)(i,i) = 1.0;
+	}
+
+	void CorrelationMatrix::set(unsigned int row, unsigned int col, double value) {
+		(*this)(row,col) = value;
+		(*this)(col,row) = value;
+	}
+}
+
