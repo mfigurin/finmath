481fcd5b56ef96af286a1bc81ed17484101a649b
mfigurin <mikhail.figurin@gmail.com>
2016-09-04 11:22:35 +0300

    put in order

--- solution/Finmath/finmath.cpp
+++ solution/Finmath/finmath.cpp
@@ -6,6 +6,16 @@ using namespace finmath;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	tm trade_date;
+	tm final_date;
+	DTUtils::set_tm_fields(&trade_date, 2016, 1, 25);
+	DTUtils::set_tm_fields(&final_date, 2019, 1, 25);
+	ContractCalendar calendar(CalendarMode::CALENDAR_DAYS, trade_date, final_date);
+
+	double notional_amount = 400.00;
+	double short_interest_rate = 0.03;
+	double knock_in_event_percentage = 0.72;
+
 	std::vector<Share> basket;
 	basket.push_back(Share("001_HK", "HKD", 17.4985, 0.03, 0.2));
 	basket.push_back(Share("002_HK", "HKD", 17.1770, 0.03, 0.2));
@@ -15,24 +25,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	matrix.set_correlation(0,1, 0.35);
 	matrix.set_correlation(0,2, -0.4);
 	matrix.set_correlation(1,2, 0.1);
-
-	tm trade_date;
-	tm final_date;
-	DTUtils::set_tm_fields(&trade_date, 2016, 1, 25);
-	DTUtils::set_tm_fields(&final_date, 2019, 1, 25);
-
-	ContractCalendar calendar(CalendarMode::CALENDAR_DAYS, trade_date, final_date);
 	CorrelationGenerator correlationGenerator(matrix, NormalDistribution());
 
-	Simulator sim(calendar, 400.00, 0.03, basket, 0.72, correlationGenerator);
-
-	std::cout << sim;
+	Simulator sim(calendar, notional_amount, short_interest_rate, basket, knock_in_event_percentage, correlationGenerator);
 
 	sim.set_sample_count(100);
-	//sim.suppress_payments(false, true);
+	//sim.suppress_payments(true, false);
 	//sim.save_iteration_data(0, "data.csv");
 	//sim.check_knock_in_event(false);
 	//sim.jump_to_final_date(false);
-
-	std::cout << "Present value: " << sim.simulate_present_value() << std::endl;	
+	
+	std::cout << "\n*** Simulation settings ***\n\n";
+	std::cout << sim;
+	std::cout << "\n*** Simulation started ***\n\n";
+	std::cout << "\nPresent value: " << sim.simulate_present_value() << std::endl;	
+	std::cout << "\n*** Simulation finished ***\n";
 }
